6f283b6b605707cabd929ae4794c2cac03fabd77
LJAMINE <aminelmgrmj@gmail.com>
2025-06-30 14:20:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello, !");
+//        System.out.println("hi my name is amine");
+//
+
+        double age = 21.3;
+        int year = 2023;
+        char grade = 'A';
+        char symbol = '!';
+        boolean isAlive = true;
+        String name="amine ";
+
+        if (!isAlive) {
+            System.out.println("amine is alive");
+        } else {
+            System.out.println("amine is not alive");
+        }
+
+        System.out.println(isAlive);
+        System.out.println(grade);
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println("the year is " + year);
+        System.out.println(name);
+
+
+
+    }
+}
